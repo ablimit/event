9efcb169fa2c63bcceff9e131c469e0542a4a872
flashever <flashever1986@gmail.com>
2012-10-04 00:49:41 -0300

    Update wikipedia/rawwikiparser/parseXml.java

--- wikipedia/rawwikiparser/parseXml.java
+++ wikipedia/rawwikiparser/parseXml.java
@@ -1,1 +1,61 @@
+import java.io.IOException;
+import java.util.*;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class parseXml {
+
+  public static class Map extends MapReduceBase implements
+			Mapper<Text, Text, Text, Text> {
+
+		public void map(Text key, Text value,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+
+			// regxtry r = new regxtry(key.toString());
+
+			String tmp = key.toString();
+			if (tmp != null) {
+				tmp = tmp.replaceAll("\\r", " ");
+				tmp = tmp.replaceAll("\\n", " ");
+				output.collect(new Text(tmp), value);
+			}
+
+		}
+	}
+
+	public static class Reduce extends MapReduceBase implements
+			Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter reporter)
+				throws IOException {
+			while (values.hasNext()) {
+				output.collect(key, values.next());
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(parseXml.class);
+		conf.setJobName("wikiparse");
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(Text.class);
+
+		conf.setMapperClass(Map.class);
+		conf.setCombinerClass(Reduce.class);
+		conf.setReducerClass(Reduce.class);
+
+		conf.setInputFormat(StreamWikiDumpInputFormat.class);
+		// conf.setOutputFormat(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(conf, new Path(args[0]));
+		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+		JobClient.runJob(conf);
+	}
+}
